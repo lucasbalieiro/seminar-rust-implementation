0544e48f97dd28cb0ae9972fc5d3336622e9b214
Lucas Balieiro <github@lucasbalieiro.dev>
2025-04-30 16:27:22 -0400

    feat: add database support

--- src/cli.rs
+++ src/cli.rs
@@ -5,7 +5,7 @@ use clap::Parser;
 #[command(about = "A CLI for interacting with the Bitcoin Seeder", long_about = None)]
 pub struct Cli {
     /// Host to connect to (default: seed.bitcoin.sipa.be)
-    #[arg(long, default_value = "seed.bitcoin.sipa.be")]
+    #[arg(long, default_value = "188.40.233.17")]
     pub host: String,
 
     /// Port to connect to (default: 8333)
@@ -23,4 +23,8 @@ pub struct Cli {
     /// Timeout for connection attempts in seconds (default: 10, minimum: 1)
     #[arg(long, default_value_t = 10, value_parser = clap::value_parser!(u64).range(1..))]
     pub timeout: u64,
+
+    /// Filter peers by service flags (e.g., compact blocks, address relay, bloom filters)
+    #[clap(long)]
+    service_flags: Option<u64>,
 }
